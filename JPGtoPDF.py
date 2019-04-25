2efbcbca5764056cb0fc65160ca6e6220cc83b77
Efim KLimov <efim.klimov.95@gmail.com>
2019-04-25 22:30:54 +0300

    Rework algorithm for image convertion

--- JPGtoPDF.py
+++ JPGtoPDF.py
@@ -1,15 +1,45 @@
 import os
+import shutil
+from fpdf import FPDF
 from PIL import Image
 
-path = "C:\\input\\Chapter 00"
-destpath = "C:\\output"
+inPath = "C:\\input"
+outPath = "C:\\output"
 
-for infile in os.listdir(path):
-    outfile = destpath + os.path.splitext(infile)[0] + ".pdf"
-    current = path + infile
 
-    if infile != outfile:
-        im = Image.open(current)
-        dpi = im.info['dpi']
+def delete_folder(pth) :
+    for sub in pth.iterdir() :
+        if sub.is_dir() :
+            delete_folder(sub)
+        else :
+            sub.unlink()
+    pth.rmdir()
 
-        im.save(outfile, "PDF", resolution=100.0)
+
+def makePdf(pdfFileName, listPages, dir = ''):
+    cover = Image.open(dir + "\\" + str(listPages[1]))
+    width, height = cover.size
+
+    # regularPage = Image.open(dir + "\\" + str(listPages[10]))
+    # reg_width, reg_height = regularPage.size
+
+    pdf = FPDF(unit="pt", format=[width, height])
+
+    # Pop first and last images out of the list
+    # listPages.pop(0)
+    # listPages.pop()
+
+    for page in listPages:
+        pdf.add_page()
+        pdf.image(dir + "\\" + str(page), 0, 0)
+
+    pdf.output(outPath + "\\" + pdfFileName + ".pdf", "F")
+
+    cover.close()
+
+    # Remove the image directory
+    shutil.rmtree(dir, ignore_errors=True)
+
+
+for chapterName in os.listdir(inPath):
+    makePdf(chapterName, os.listdir(inPath + "\\" + chapterName), inPath + "\\" + chapterName)
